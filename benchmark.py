5562916c006359a2fe637459110df82a48c3cf9e
nikitaNotvas <nvasiuta@brooklynsteamcenter.org>
2023-09-19 09:50:48 -0400

    excersizes

---
+++ benchmark.py
@@ -0,0 +1,39 @@
+#excersize 1:
+print("nikita")
+
+
+#exersize 2
+FavoriteShow=("DrHouse")
+print(FavoriteShow)
+
+
+#exersize 3
+
+age=(16)
+height=(182)#in cm not ft
+
+print(age*height)
+
+
+#excersize 4
+print("Im going to break")
+
+
+#excersize 5
+fawFood=("pizza","cookies","chips")
+print(fawFood)
+
+#excersize 6 
+
+
+notage=int(input("tell me your age "))
+if notage<= 21 :print("you can rent car")
+else :print("your beat")
+#excersize 7
+
+
+notname=input("tell me your name ")
+print("Hello "+str(notname))
+#excersize 8
+
+#excersize 9 
