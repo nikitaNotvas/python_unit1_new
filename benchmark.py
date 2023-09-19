961e78b72c7029fdbd81cdabaaf31d144b02919e
nikitaNotvas <nvasiuta@brooklynsteamcenter.org>
2023-09-19 10:48:27 -0400

    fixed all of excersizes

--- benchmark.py
+++ benchmark.py
@@ -20,14 +20,13 @@ print("Im going to break")
 
 
 #excersize 5
-fawFood=("pizza","cookies","chips")
-print(fawFood)
+fawFood=["pizza","cookies","chips"]
 
 #excersize 6 
 
 
 notage=int(input("tell me your age "))
-if notage<= 21 :print("you can rent car")
+if notage>= 21 :print("you can rent car")
 else :print("your beat")
 #excersize 7
 
@@ -36,4 +35,10 @@ notname=input("tell me your name ")
 print("Hello "+str(notname))
 #excersize 8
 
+for i in fawFood: 
+    print(i)
+
 #excersize 9 
+x = 1 
+while x == 2:
+    print('equal >;]')
