74774bd6fc23ffbf5450ced0e7fa1ead93b6ff1b
nikitaNotvas <nvasiuta@brooklynsteamcenter.org>
2023-10-03 10:49:04 -0400

    almost finished whimisc_whilews

---
+++ whimsical_whiles.py
@@ -0,0 +1,51 @@
+
+#1. Simple Counter:
+#Write a program that uses a while loop to print numbers from 1 to 10.
+c=1
+while c<11:
+    print(c)
+    c+=1
+
+print()
+print()
+#2. Countdown:
+#Write a program that uses a while loop to print numbers from 10 to 1 in descending order.
+c=10
+while c>0:
+    print(c)
+    c-=1
+
+
+#3. Factorial Calculation:
+#Write a program that calculates the factorial of a given number using a while loop.
+print()
+print()
+nn=9
+nnn=1
+
+while nn>1:
+    nnn= nnn*nn
+    print(nnn)
+    nn-=1
+
+
+
+#4. Password Guessing Game:
+#Create a simple password guessing game using a while loop. Ask the user to guess a predefined password and provide appropriate feedback.
+
+aa=input("enter paswrod: ")
+while aa !="za pasword":
+    print("try one more time")
+    aa=input("enter paswrod: ")
+if aa == "za pasword":
+    print("good job")
+
+
+#5. Sum of Digits:
+#Write a program that calculates the sum of the digits of a given number using a while loop.
+
+
+
+
+#6. Fibonacci Series:
+#Write a program that prints the first n numbers in the Fibonacci sequence using a while loop.
