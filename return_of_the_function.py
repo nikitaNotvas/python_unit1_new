79182cb66a34852269f623a7c11b1f11c2a37046
nikitaNotvas <nvasiuta@brooklynsteamcenter.org>
2023-10-17 10:52:07 -0400

    finished try_exept

---
+++ return_of_the_function.py
@@ -0,0 +1,23 @@
+
+#Task 1: Calculate the Square of a Number
+#Write a function that takes an integer as an argument and returns its square.
+
+
+
+
+#Task 2: Calculate the Area of a Rectangle:
+#Write a function that takes the length and width of a rectangle and returns its area.
+
+
+
+
+#Task 3: Convert Temperature from Celsius to Fahrenheit:
+#Write a function that takes a temperature in Celsius and returns 
+#the equivalent temperature in Fahrenheit using the correct formula
+
+
+
+
+#Task 4: Calculate the Average of Numbers:
+#Write a function that takes a list of numbers 
+#and returns the average (mean) of those numbers.
